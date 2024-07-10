6fcb17f4bd599d3bdcd45121495fc71102642a81
Hunter <hunvicter@gmail.com>
2024-07-10 14:26:12 +0700

    feat/answer

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,14 @@
 function twoSum(numbers, target) {
 	// เริ่มเขียนโค้ดตรงนี้จ้า
+	let resultSum =[]
+	for(let i=0;i<numbers.length;i++){
+		for( let j=i;j<=numbers.length-1;j++){
+			if(numbers[i]+numbers[j] === target){
+				resultSum.push(i,j)
+				return resultSum
+			}
+		}
+	}	
 };
+console.log(twoSum([2,7,11,15],9)); 
+
